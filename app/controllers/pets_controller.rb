94e78e8f44592175d428eab30b060fd10511560a
Brittany Morris <brittmmendez@gmail.com>
2018-01-29 19:53:06 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,8 +33,9 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
-      @pet.owner=params["owner"]["name"]
+      @pet.owner=Owner.create(name: params["owner"]["name"])
     end
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
   end
